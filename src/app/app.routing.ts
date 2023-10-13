c580dd52689674454e17c34e92154e2726b54212
erik1110 <oceanuheart@gmail.com>
2023-10-13 18:21:15 +0800

    [feat] add cors

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,27 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/test', (req, res, next) => {
-    res.send('test!');
+    next('error page.');
 });
 
+router.get('/data/error', (req, res, next) => {
+    // Fake API
+    const getProfile = new Promise((resolve, reject) => {
+      setTimeout(() => resolve({ name: 'HAO', age: 22 }), 100);
+    });
+    const getFriends = new Promise((resolve, reject) => {
+      setTimeout(() => resolve([]), 120);
+    });
+    const errorRequest = new Promise((resolve, reject) => {
+      setTimeout(() => reject('Oops!'), 2000);
+    });
+  
+    getProfile
+    .then(profile => getFriends)
+    .then(friends => errorRequest)
+    .then(() => res.send('GoGoGo!'))
+    .catch(err => next(err));
+  });
+
 export default router;
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
-
+import cors from 'cors';
 import appRoute from './app/app.routing';
 
 const app = express();
@@ -12,6 +12,14 @@ app.get('/', (req, res, next) => {
     res.send('Hello, World!!');
 });
 
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  };
+
+app.use(cors(corsOptions));
 app.use('/', appRoute);
 
 app.listen(process.env.PORT, () => console.log(`http server is running at port ${ process.env.PORT }.`));
